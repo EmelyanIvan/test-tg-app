02f0c54cf2b2bd1b79bf0f5a95bfec7ff52e7813
iemelyanenko <ivan.yemelyanenka@aetsoft.net>
2025-05-16 22:55:08 +0300

    T

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import WebApp from "@twa-dev/sdk";
 function App() {
   return (
     <div className="App">
+      <h1>Test app</h1>
       <h1>Привет, {WebApp.initDataUnsafe.user?.first_name}!</h1>
       <button onClick={() => WebApp.close()}>Закрыть</button>
     </div>
